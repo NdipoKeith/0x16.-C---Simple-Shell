dec159051f6c8e23d14ec8b42fc71a271ed23152
emmanueloamen6 <emmanueloamen6@gmail.com>
2023-07-19 11:34:32 -0700

    simple_shell

--- execve.c
+++ execve.c
@@ -4,21 +4,29 @@
 int main()
 {
 	pid_t pid;
-	char *argv[] = {"/bin/ls",  "-l", NULL};
+	pid_t ppid = getpid();
+	int i = 0;
+	char *argv[] = {"/bin/ls",  "-l",  NULL};
+	printf("This is the parent pid %d\n", ppid);
 	
-	pid = fork();
-	if (pid == -1)
-		return (1);
-	if (pid == 0)
+	while (1)
 	{
-		int val = execve(argv[0], argv, NULL);
-		if (val == -1)
-			perror("Error");
-	}
-	else
-	{
-		wait(NULL);
-		printf("Done on wih execve\n");
+		if (i == 5)
+			break;
+		pid = fork();
+		if (pid == -1)
+			return (1);
+		if (pid == 0)
+		{
+			int val = execve(argv[0], argv, NULL);
+			if (val == -1)
+				perror("Error");
+		}
+		else
+		{
+			wait(NULL);
+			printf("Done on with execve\n");
+		}i++;
 	}
 	return (0);
 }
